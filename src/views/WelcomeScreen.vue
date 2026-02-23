c7e4ce1120a1e409b2bc7a49c8dd35f5a210fa31
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2026-02-23 19:35:51 +0100

    first commit

---
+++ src/views/WelcomeScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-content class="welcome">
+    <div class="background"></div>
+
+    <h1 class="title">FISH COLLECTOR: IDLE</h1>
+
+    <ion-button class="menu-button">Sign In</ion-button>
+    <ion-button class="menu-button">Log In</ion-button>
+    <ion-button class="menu-button">Play</ion-button>
+
+    <!-- peces decorativos -->
+    <img class="fish anchovy" src="@/assets/img/Anchovy.png" />
+    <img class="fish puffer" src="@/assets/img/Pufferfish.png" />
+    <!-- etc -->
+  </ion-content>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* aquí adaptarías tu CSS */
+</style>
